3d6196952140d3521f2f711a8ee69d9d18962fba
ferreiracamilo <camiloferreira2009@hotmail.com>
2021-11-10 02:07:37 -0300

    Erase unnecessary import within BasePage class

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
