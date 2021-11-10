38a547101f3b97c744803d08721df7bd99628699
ferreiracamilo <camiloferreira2009@hotmail.com>
2021-11-09 23:45:31 -0300

    init

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,23 @@
+package runner;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "steps",
+    plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","json:target/cucumber-reports.json"},
+    monochrome = true
+    //tags = {"@Grid","@Smoke"} -> in case is wanted to specified which tag needs to be executed without passing a tag through terminal with gradle
+)
+
+public class Runner{
+    @AfterClass
+    public static void cleanDriver(){
+        //close method to be called
+    }
+}
